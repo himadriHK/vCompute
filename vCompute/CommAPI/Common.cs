7bb4fe45fb0da4356d24786c09d103b4b428b699
HimadriHK <himadri.hk@gmail.com>
2018-05-07 20:29:02 +0530

    minor updates and bug fixes

--- vCompute/CommAPI/Common.cs
+++ vCompute/CommAPI/Common.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Web.Script.Serialization;
 using System.Text;
@@ -9,6 +9,7 @@ using CodeLoader;
 using System.Security;
 using System.Security.Policy;
 using System.Diagnostics;
+using System.Runtime.Remoting;
 
 namespace CommAPI
 {
@@ -16,16 +17,16 @@ namespace CommAPI
 	public class Common
 	{
 		
-		private const int payloadSize = 1024;
-		private const double assemblySize = 1000.0;
-		private Dictionary<int, Payload> TaskList;
+		private const int payloadSize = 2048;
+		private const double assemblySize = 500.0;
+		private Dictionary<string, Payload> TaskList;
 		public Loader codeLoader;
 		private int timeOut=30;
 
 		public Common(string codeBinaryFilePath)
 		{
 			codeLoader = new Loader(codeBinaryFilePath);
-			TaskList = new Dictionary<int, Payload>();
+			TaskList = new Dictionary<string, Payload>();
 		}
 
 		public string executeAssembly(string assemblyName, string param)
@@ -41,29 +42,22 @@ namespace CommAPI
 
 			AppDomainSetup ads = new AppDomainSetup();
 			ads.ApplicationBase = AppDomain.CurrentDomain.BaseDirectory;
-			AppDomain tempDomain = AppDomain.CreateDomain("assemblyName" + new Random().NextDouble().ToString(), e, ads, pset, null);
+			AppDomain tempDomain = AppDomain.CreateDomain(assemblyName + new Random().NextDouble().ToString(), e, ads, pset, null);
+
+			tempDomain.Load("CodeLoader");
 
 			if (assemblyName.ToLower() == "discovery")
 				return string.Join(",", codeLoader.codeDictionary.getAssemblyList());
 
+			ObjectHandle handle = Activator.CreateInstanceFrom(tempDomain, typeof(Sandboxer).Assembly.ManifestModule.FullyQualifiedName,
+typeof(Sandboxer).FullName);
+
+			Sandboxer newDomainInstance = (Sandboxer)handle.Unwrap();
+
 			byte[] assemblyBinary = codeLoader.codeDictionary.readAssembly(assemblyName);
-			Assembly asm = tempDomain.Load(assemblyBinary);
-			CodeInterface assemblyObj = null;
 
-			foreach (Type t in asm.GetTypes())
-				if (t.GetInterface("CodeInterface") != null)
-					assemblyObj = (CodeInterface)Activator.CreateInstance(t);
+			object output = newDomainInstance.ExecuteNewAssembly(assemblyBinary, param);
 
-			object output = string.Empty;
-
-			try
-			{
-				output = assemblyObj.DoWork(param);
-			}
-			catch(Exception ex)
-			{
-				output = ex.Message;
-			}
 			return new JavaScriptSerializer().Serialize(output);
 		}
 
@@ -88,11 +82,9 @@ namespace CommAPI
 		{
 			string serializedData = preparePayload(payload);
 			byte[] data = Encoding.ASCII.GetBytes(serializedData);
-
-			lock (stream)
-			{
-				stream.Write(data, 0, payloadSize);
-			}
+			stream.Write(data, 0, payloadSize);
+			if(payload.command!=CommandType.STATUS)
+			Debug.Print("Sending " + Enum.GetName(typeof(CommandType), payload.command));
 		}
 
 		public byte[][] splitBytes(byte[] assemblyBytes)
@@ -122,36 +114,42 @@ namespace CommAPI
 			return splitData;
 		}
 
-		public object blockUntilResult(int runId)
+		public object blockUntilResult(string runId)
 		{
-			Payload resultPayload = TaskList[runId];
+	
 			Stopwatch timer = new Stopwatch();
 			timer.Start();
-			while(true)
+			TimeSpan span = new TimeSpan(0, 0, timeOut);
+			while (true)
 			{
+				Payload resultPayload = TaskList[runId];
 				if (resultPayload == null || resultPayload.isAppend == true)
 					continue;
 
-				if (resultPayload.isAppend == false && resultPayload.remainingPayloads == 0 && timer.Elapsed==new TimeSpan(0,0,timeOut))
-					break;
+				if (timer.Elapsed > span)
+					return "";
+
+				if (resultPayload.isAppend == false && resultPayload.remainingPayloads == 0 && timer.Elapsed <= span)
+				{
+					timer.Stop();
+					return new JavaScriptSerializer().Deserialize<object>(resultPayload.jsonOutput);
+				}
 			}
-			timer.Stop();
-			if (resultPayload != null && resultPayload.isAppend == false && resultPayload.remainingPayloads == 0)
-				return new JavaScriptSerializer().Deserialize<object>(resultPayload.jsonOutput);
-			else
-				return null;
 		}
 
-		public void storeResult(string assemblyName, int runId, string jsonOutput, bool isAppend, int remainingPayloads)
+		public void storeResult(string assemblyName, string runId, string jsonOutput, bool isAppend, int remainingPayloads)
 		{
 			lock (TaskList)
 			{
-				Payload partialResult = TaskList[runId];
+				Payload partialResult = null;
+				if (TaskList.ContainsKey(runId))
+					partialResult = TaskList[runId];
+
 				if (partialResult == null)
 				{
 					partialResult = new Payload();
 					partialResult.assemblyName = assemblyName;
-					partialResult.runId = runId;
+					partialResult.runId = runId.ToString();
 					partialResult.jsonOutput = jsonOutput;
 					partialResult.isAppend = isAppend;
 					partialResult.remainingPayloads = remainingPayloads;
@@ -167,7 +165,7 @@ namespace CommAPI
 			}
 		}
 
-		public void addToTaskList(int runId)
+		public void addToTaskList(string runId)
 		{
 			lock(TaskList)
 			TaskList.Add(runId, null);
@@ -175,12 +173,14 @@ namespace CommAPI
 
 		public Payload getPacket(NetworkStream stream)
 		{
-			lock(stream)
+			//lock(stream)
 			{
 				byte[] buffer = new byte[payloadSize];
 				stream.Read(buffer, 0, payloadSize);
 				string serializedData = Encoding.ASCII.GetString(buffer);
 				Payload output = preparePayload(serializedData);
+				if (output.command != CommandType.STATUS)
+					Debug.Print("Receiving " + Enum.GetName(typeof(CommandType), output.command));
 				return output;
 			}
 		}
@@ -194,7 +194,7 @@ namespace CommAPI
 		public string clientId;
 		public string payloadId;
 		public int remainingPayloads;
-		public int runId;
+		public string runId;
 		public bool isAppend;
 		public string assemblyName;
 		public byte[] assemblyBytes;
@@ -212,6 +212,31 @@ namespace CommAPI
 		public int Compare(Payload x, Payload y)
 		{
 			return x.remainingPayloads.CompareTo(y.remainingPayloads);
+		}
+	}
+
+	class Sandboxer : MarshalByRefObject
+	{
+		public object ExecuteNewAssembly(byte[] rawAssm, object param)
+		{
+			Assembly asm = Assembly.Load(rawAssm);
+			CodeInterface assemblyObj = null;
+
+			foreach (Type t in asm.GetTypes())
+				if (t.GetInterface("CodeInterface") != null)
+					assemblyObj = (CodeInterface)Activator.CreateInstance(t);
+
+			object output = string.Empty;
+
+			try
+			{
+				output = assemblyObj.DoWork(param);
+			}
+			catch (Exception ex)
+			{
+				output = ex.Message;
+			}
+			return output;
 		}
 	}
 }
