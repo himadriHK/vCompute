0686ef1506f52f9762ac402e5045f99169181317
E1087633 <E1087633@AP-BGLR-LP0360>
2018-05-10 13:32:48 +0530

    Revert "Compile Assembly"
    This reverts commit 7a06712359894562b0805378908af033cf6ac09f.

--- vCompute/vComputeClient/Form1.Designer.cs
+++ vCompute/vComputeClient/Form1.Designer.cs
@@ -28,48 +28,20 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(12, 3);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.textBox1.Size = new System.Drawing.Size(659, 422);
-			this.textBox1.TabIndex = 0;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(43, 513);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "compile";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(735, 653);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.textBox1);
+			this.ClientSize = new System.Drawing.Size(786, 486);
 			this.Name = "Form1";
 			this.Text = "vCompute Client";
-			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.Button button1;
 	}
 }
 
--- vCompute/vComputeClient/Form1.cs
+++ vCompute/vComputeClient/Form1.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,50 +15,6 @@ namespace vComputeClient
 		public Form1()
 		{
 			InitializeComponent();
-		}
-
-		private void Form1_Load(object sender, EventArgs e)
-		{
-
-		}
-
-
-		public byte[] ComplieAssembly(string sourceFile)
-		{
-
-			var provider = CodeDomProvider.CreateProvider("CSharp");
-			CompilerParameters cp = new CompilerParameters();
-			cp.GenerateExecutable = false;
-			cp.GenerateInMemory = true;
-			cp.ReferencedAssemblies.Add("CodeLoader.dll");
-			
-			CompilerResults result=provider.CompileAssemblyFromSource(cp, sourceFile);
-			if (result.Errors.HasErrors)
-			{
-				MessageBox.Show("Errors: "+result.Errors[0]);
-				return null;
-			}
-			else
-			{
-				using (MemoryStream stream = new MemoryStream())
-
-				{
-
-					BinaryFormatter formatter = new BinaryFormatter();
-
-					formatter.Serialize(stream, result.CompiledAssembly);
-
-					return stream.ToArray();
-
-				}
-			}
-		}
-
-		private void button1_Click(object sender, EventArgs e)
-		{
-			var sourceFile = textBox1.Text;
-			var compliedCodeInByte = ComplieAssembly(sourceFile);
-
 		}
 	}
 }
